2c1f932f3f8e0e00178c010b6180d9119f9630d2
sunyk <2876053639@qq.com>
2024-02-11 22:22:52 +0800

    问题修复：修复eslint配置报错问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   'env': {
     'browser': true,
     'es2021': true,
+    'node': true,
   },
   'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-essential'],
   'overrides': [
     {
-      'env': {
-        'node': true,
-      },
+      // 'env': {
+      //   'node': true,
+      // },
       'files': ['.eslintrc.{js,cjs}'],
       'parserOptions': {
         'sourceType': 'script',
