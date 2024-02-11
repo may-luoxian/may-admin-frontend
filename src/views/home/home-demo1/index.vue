485d03f51beb6f134a49e937044b967bd9f902c9
sunyk <2876053639@qq.com>
2024-02-11 22:18:54 +0800

    功能新增：初始化日历图

--- src/views/home/home-demo1/index.vue
+++ src/views/home/home-demo1/index.vue
@@ -22,14 +22,28 @@
     <el-divider></el-divider>
     <!-- 主体区域 -->
     <div class="home-main">
-      <el-table> </el-table>
+      <el-dropdown class="float-right mr-2">
+        <span class="el-dropdown-link">
+          Dropdown List
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item v-for="item in yearList" :key="item">{{ item }}</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+      <div ref="echartRef" class="h-full w-full"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { toRefs, onMounted, ref } from 'vue';
 import SvgIcon from '@/components/icon/src/SvgIcon.vue';
+import * as echarts from 'echarts';
 
 interface Props {
   title: string;
@@ -40,6 +54,49 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const { title } = toRefs(props);
+
+const yearList = ref(['2021', '2022', '2023', '2024']);
+
+const currentYear = ref();
+const echartRef = ref();
+
+onMounted(() => {
+  init();
+});
+
+/**
+ * 初始化日历图
+ */
+const init = () => {
+  const option = {
+    visualMap: {
+      show: false,
+      min: 0,
+      max: 10000,
+    },
+    calendar: {
+      range: '2017',
+    },
+    series: {
+      type: 'heatmap',
+      coordinateSystem: 'calendar',
+      data: getVirtualData('2017'),
+    },
+  };
+  const echart = echarts.init(echartRef.value);
+  echart.setOption(option);
+};
+
+const getVirtualData = (year: string) => {
+  const date = +echarts.time.parse(year + '-01-01');
+  const end = +echarts.time.parse(year + '-12-31');
+  const dayTime = 3600 * 24 * 1000;
+  const data = [];
+  for (let time = date; time <= end; time += dayTime) {
+    data.push([echarts.time.format(time, '{yyyy}-{MM}-{dd}', false), Math.floor(Math.random() * 10000)]);
+  }
+  return data;
+};
 </script>
 
 <style lang="scss" scoped>
