4ea2f8eba1d82629f49d400752a932f3fc6f3191
sunyk <2876053639@qq.com>
2024-03-21 11:24:58 +0800

    问题修复：修复风格初始化问题

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useAppStore } from '@/stores/modules/app';
+/**
+ * 项目初始化
+ * 1. 初始化风格
+ */
+const init = () => {
+  let themeSetting = false;
+  //auto 亮色 dark暗黑
+  if (localStorage.getItem('may-blog-theme')) {
+    themeSetting = ['auto', 'light'].includes(String(localStorage.getItem('may-blog-theme'))) ? false : true;
+  }
+  useAppStore().themeConfig.theme = themeSetting;
+};
+
+onMounted(() => {
+  init();
+});
+</script>
 
 <template>
   <RouterView />
--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -1,13 +1,11 @@
 import { defineStore } from 'pinia';
 import { i18n } from '@/locales';
 
-//auto 亮色 dark暗黑
-const themeSetting = String(localStorage.getItem('may-blog-theme')) === 'auto' ? false : true;
 export const useAppStore = defineStore('app', {
   state: () => {
     return {
       themeConfig: {
-        theme: themeSetting,
+        theme: false,
       },
     };
   },
